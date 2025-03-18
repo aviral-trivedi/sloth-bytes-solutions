ef4e2780d7757acb66395206cff50ab2d3092a36
Aviral <192993778+aviral-trivedi@users.noreply.github.com>
2025-03-18 12:00:56 +0530

    Fix: Stop-Copy-Pasting: Added Code To Support Hello

--- solutions/stop-copy-pasting.py
+++ solutions/stop-copy-pasting.py
@@ -1,9 +1,9 @@
-def turnCalc(number : int):
+def turnCalc(number):
     """
     Turns the `number` 180 degrees according to a set table 
 
     Args:
-        number (int) : the integral number to turn
+        number : the number to turn
     
     Returns:
         turned_num (str) : the rotated number.
@@ -12,12 +12,10 @@ def turnCalc(number : int):
     data_table = {1:"I", 2:"Z", 3:"E", 4:"H", 5:"S", 6:"G", 7:"L", 8:"B", 9:"-", 0:"O"}
     turned_num = ""
 
-    #Ignore the int -> str -> int conversion for now 
-
     for digit in str(number):
         turned_num += data_table[int(digit)]
 
     #Since `turned_num` is a string we flip the result in the end
     return turned_num[::-1]
 
-print(turnCalc(8008))
+print(turnCalc('07734'))
